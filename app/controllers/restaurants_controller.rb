2096f8f2fadfbded30f9bc0d06bead07bacc79de
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-16 13:51:55 -0300

    add methods to controller and implement #new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,19 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def strong_params
+  end
 end
